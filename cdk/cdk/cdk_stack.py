1d3273a3dbe1f06461bd4a918c22de994901f2b1
Kelvinskell <kelvinskelll@gmail.com>
2023-11-24 01:47:23 +0100

    Create event source and bind to lambda

--- cdk/cdk/cdk_stack.py
+++ cdk/cdk/cdk_stack.py
@@ -14,9 +14,22 @@ class CdkStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # create SQS queue
         queue = sqs.Queue(
             self, "CdkQueue",
             visibility_timeout=Duration.seconds(300),
         )
 
+        # Create lambda function
+        sqs_lambda = lambda_.Function(self, "APISQSLambda",
+                                      handler='lambda_handler.handler',
+                                      runtime=lambda_.Runtime.PYTHON_3_10,
+                                      code=lambda_.Code.from_asset('lambda')
+                                      )
+        
+        # Create SQS event source
+        sqs_event_source = lambda_event_sources.SqsEventSource(queue)
+
+        # Add SQS event source to lambda
+        sqs_lambda.add_event_source(sqs_event_source)
         
